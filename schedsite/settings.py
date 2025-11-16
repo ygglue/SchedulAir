5d7b6878bcf0079726e594ce788a0d91e802f92b
ygglue <159265464+ygglue@users.noreply.github.com>
2025-11-17 01:28:29 +0800

    edited database settings and edited vercel.json

--- schedsite/settings.py
+++ schedsite/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'schedsite.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default':  dj_database_url.config(default=env('DATABASE_URL'), conn_max_age=600, ssl_require=True)
+    'default':  dj_database_url.config(default=env('DATABASE_URL'), conn_max_age=0, ssl_require=True)
 }
 
 
